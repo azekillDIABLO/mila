338cb0f8b4ecd959183c261805d6e85c63a95d1b
Tai @ Flex <dch.tai@gmail.com>
2016-09-03 04:32:24 +0100

    update modname

--- sheep/init.lua
+++ sheep/init.lua
@@ -1,17 +1,17 @@
 --try to register the first entity.
 
-mila:add_entity("mila:sheep", { -- define and access functions on Lua tables using the ":" operator
-	hp_max = 30,
+mila:add_entity("milasheep:sheep", { -- define and access functions on Lua tables using the ":" operator
+	hp_max = 10,
 	collisionbox = {-0.5, -0.5, -0.5,  0.5, 0.5, 0.5},
 	mesh = "sheep.x",
-	textures = {"default_dirt.png"},
+	textures = {"sheep.png"},
 	speed = 1,
 	view_range = 5,
 })
    
 --then the egg
 
-mila:add_egg("mila:sheep", {
+mila:add_egg("milasheep:sheep", {
 	description = "Sheep Egg",
 	inventory_image = "sheep.png",
 	wield_image = "sheep.png",

620f92b11ae8fe7547cbc00b2e9dc13806693707
Tai @ Flex <dch.tai@gmail.com>
2016-09-04 18:38:27 +0100

    adjusted colbox and cust fall speed

--- sheep/init.lua
+++ sheep/init.lua
@@ -2,12 +2,13 @@
 
 mila:add_entity("milasheep:sheep", { -- define and access functions on Lua tables using the ":" operator
 	hp_max = 10,
-	collisionbox = {-1, -1, -1,  1, 1, 1},
+	collisionbox = {-0.5, -0.1, -0.5,  0.5, 1.1, 0.5},
 	visual_size = 1,
 	mesh = "sheep.x",
 	visual = "mesh",
 	textures = {"sheep.png^sheep_wool.png"},
 	speed = 2,
+	fall_speed = -5,
 	view_range = 5,
 	rotate = 90,
 })

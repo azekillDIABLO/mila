3353ac9c66db1fac406580e67fca06f6276c4737
Tai @ Flex <dch.tai@gmail.com>
2017-01-30 11:37:44 +0000

    change textures, add new def for new features

--- sheep/init.lua
+++ sheep/init.lua
@@ -6,17 +6,22 @@ mila:add_entity("milasheep:sheep", { -- define and access functions on Lua table
 	visual_size = 1,
 	mesh = "sheep.x",
 	visual = "mesh",
-	textures = {"sheep.png^sheep_wool.png"},
+	textures = {"mila_sheep.png^mila_sheep_wool.png"},
 	speed = 2,
-	fall_speed = -5,
+	gravity = 5,
 	view_range = 5,
-	rotate = 90,
+	range = 2,
+	rotate = 0,
+	status = "passive",
 })
    
 --then the egg
 
 mila:add_egg("milasheep:sheep", {
 	description = "Sheep Egg",
-	inventory_image = "sheep.png^sheep_wool.png",
-	wield_image = "sheep.png^sheep_wool.png",
+	inventory_image = "mobs_sheep.png",
 })
+
+-- and a spawn
+
+mila:add_spawn("milasheep:sheep")

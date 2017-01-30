b362de900b0a3023b6dc4a9e29d12c59f7f168e2
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2017-01-30 13:24:59 +0100

    setting added + orthograph

--- mila/settings.lua
+++ mila/settings.lua
@@ -7,13 +7,14 @@ This is a .lua file, so comment is needed.
 --say that we are loading
 print("M.I.L.A " ..mila.version..": Settings file found, loading...")
 
--- 1 for realistic blood, 2 for blood trail, 3 for the masive butchery and 4 to disable:
+-- 1 for realistic blood, 2 for blood trail, 3 for the massive butchery and 4 to disable:
 mila.bleed_type = 1
 
 --say true to active, other words are counted as false:
 mila.egg = true
+mila.spawning = true
 
--- step counter
+-- step counter for performance
 mila.globalcounter = 0.2
 
 

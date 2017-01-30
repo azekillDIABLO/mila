cba8d933c3bb7e72735188d2c33a997ba8e00ab5
Tai @ Flex <dch.tai@gmail.com>
2017-01-30 11:37:09 +0000

    add global counter, move variables to namespace object

--- mila/settings.lua
+++ mila/settings.lua
@@ -5,13 +5,17 @@ This is a .lua file, so comment is needed.
 ]]
 
 --say that we are loading
-print("M.I.L.A " ..version..": Settings file found, loading...")
+print("M.I.L.A " ..mila.version..": Settings file found, loading...")
 
 -- 1 for realistic blood, 2 for blood trail, 3 for the masive butchery and 4 to disable:
-bleed_type = 1
+mila.bleed_type = 1
 
 --say true to active, other words are counted as false:
-egg = true
+mila.egg = true
+
+-- step counter
+mila.globalcounter = 0.2
+
 
 --say that we are ready!
-print("M.I.L.A " ..version..": Settings file fully loaded, have fun!")
+print("M.I.L.A " ..mila.version..": Settings file fully loaded, have fun!")
